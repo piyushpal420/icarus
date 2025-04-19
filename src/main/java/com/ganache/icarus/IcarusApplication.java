1f7a1248490b9217f12ce89d732d56e8be207ad8
piyushpal420 <piyushpalgg26@gmail.com>
2025-04-19 22:06:52 +0530

    First Commit

---
+++ src/main/java/com/ganache/icarus/IcarusApplication.java
@@ -0,0 +1,13 @@
+package com.ganache.icarus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcarusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcarusApplication.class, args);
+    }
+
+}
